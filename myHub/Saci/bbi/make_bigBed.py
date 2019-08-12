bd7e872ffdf9d9901b363fe94cc3e1ff49677e32
dmatelska <dmatelska@genesilico.pl>
2019-08-12 22:23:36 +0100

    New bigbed

--- myHub/Saci/bbi/make_bigBed.py
+++ myHub/Saci/bbi/make_bigBed.py
@@ -19,23 +19,28 @@ with open(out, 'w') as f:
 		if len(tab) < 8:
 			continue
 		desc = tab[8]
+		extra = ''
+		if 'product=' in desc:
+			extra = desc.split('product=')[1].split(';')[0]
 		if tab[2] == 'CDS':
 			col = '31,119,180'
 			xid = desc.split('Parent=')[1].split(';')[0]
-			print(tab[0], tab[3], tab[4], id2name[xid], 1000, tab[6], tab[3], tab[4], col, sep='\t', file=f)
+			print(tab[0], tab[3], tab[4], id2name[xid], 1000, tab[6], tab[3], tab[4], col, extra, sep='\t', file=f)
 		elif tab[2] in ('transcript', 'tRNA', 'rRNA'):
 			col = '44,160,44'
 			xid = desc.split('Parent=')[1].split(';')[0]
-			print(tab[0], tab[3], tab[4], id2name[xid], 1000, tab[6], tab[3], tab[4], col, sep='\t', file=f)
+			print(tab[0], tab[3], tab[4], id2name[xid], 1000, tab[6], tab[3], tab[4], col, extra, sep='\t', file=f)
 		elif 'TSS' in tab[2]:
 			col = '255,127,14'
 			gene = desc.split('Gene=')[1].split(';')[0]
-			print(tab[0], tab[3], tab[4], 'TSS', 1000, tab[6], tab[3], tab[4], col, sep='\t', file=f)
+			print(tab[0], tab[3], tab[4], 'TSS', 1000, tab[6], tab[3], tab[4], col, extra, sep='\t', file=f)
 		elif 'terminator' in tab[2]:
 			col = '214,39,40'
 			gene = desc.split('Gene=')[1].split(';')[0]
-			print(tab[0], tab[3], tab[4], 'term', 1000, tab[6], tab[3], tab[4], col, sep='\t', file=f)
+			print(tab[0], tab[3], tab[4], tab[2][0]+'term', 1000, tab[6], tab[3], tab[4], col, extra, sep='\t', file=f)
 
 os.system('sort -k1,1 -k2,2n %s > tmp2' % out)
-os.system('bedToBigBed -type=bed9 -tab tmp2 chrom.sizes %s' % out)
+#os.system('echo "track name=Genes type=bedDetail" > tmp1')
+#os.system('cat tmp1 tmp2 > tmp3')
+os.system('bedToBigBed -type=bed9+1 -tab tmp2 chrom.sizes %s' % out)
 os.system('rm tmp2')
